e0ade67400b1fa522a3936bbe0b915b017e4dd1e
toyjhlee <toyjhlee@gmail.com>
2023-09-08 21:54:32 +0900

    chore: install prettier v3, apply it, and add prettier-plugin-tailwindcss

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+// prettier.config.js
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindAttributes: [className],
+  //   tailwindFunctions: ['clsx', 'tw'],
+  tailwindConfig: './tailwind.config.ts',
+}
