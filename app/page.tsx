6962463e6ae6ecaeb8038435fe6f5fd84344856b
toyjhlee <toyjhlee@gmail.com>
2023-09-11 18:43:39 +0900

    fix: resolve client component event handler issue
    Added a form and assigned a submit event which led to an error:
    "Error: Event handlers cannot be passed to Client Component props."
    
    To resolve this, referenced the following solution from StackOverflow:
    - Ensure `use client` is used for client-side interactivity as all components in Next 13 are server components by default.
    
    Source: https://stackoverflow.com/questions/74471642/nextjs-13-button-onclick-event-handlers-cannot-be-passed-to-client-componen

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
+'use client'
+
+import { FormEvent } from 'react'
+
 export default function Home() {
-  return null
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" name="name" />
+      <button type="submit">Submit</button>
+    </form>
+  )
 }
