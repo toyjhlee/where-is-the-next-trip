ba2869b81baa7ed2c3b2801b9ce5adbf8ceea92f
toyjhlee <toyjhlee@gmail.com>
2023-09-12 14:18:09 +0900

    fix: Update useRouter import path for Next.js 13
    - Next.js 13 introduces Server Side Rendering by default, causing issues with traditional useRouter imports.
    - Error encountered: "Error: NextRouter was not mounted. https://nextjs.org/docs/messages/next-router-not-mounted".
    - Resolved by referencing a solution from StackOverflow: https://stackoverflow.com/questions/74421327/nextrouter-was-not-mounted-next-js.
    - Updated import from 'next/router' to 'next/navigation' for compatibility.
    
    This commit ensures smooth routing operations in the Next.js 13 environment.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 'use client'
 
 import { FormEvent } from 'react'
+import { useRouter } from 'next/navigation'
+
+// https://stackoverflow.com/questions/74421327/nextrouter-was-not-mounted-next-js
 
 export default function Home() {
   async function onSubmit(event: FormEvent<HTMLFormElement>) {
@@ -11,8 +14,26 @@ export default function Home() {
 
   return (
     <form onSubmit={onSubmit}>
+      <MonthSelector />
       <input type="text" name="name" />
       <button type="submit">Submit</button>
     </form>
+  )
+}
+
+const MonthSelector = () => {
+  const router = useRouter()
+
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedMonth = event.target.value
+    router.push(`/${selectedMonth}`)
+  }
+
+  return (
+    <select onChange={handleChange}>
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((n: number) => (
+        <option value={n} key={n}>{`${n}월`}</option>
+      ))}
+    </select>
   )
 }
