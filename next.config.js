c5ab6dbd53615743b84229cb2b699a106180c766
toyjhlee <toyjhlee@gmail.com>
2023-11-24 18:03:11 +0900

    feat(build): set basePath, assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const debug = process.env.NODE_ENV !== 'production'
-const repository = 'joonyoung-s-portfolio-page'
+const repository = 'where-is-the-next-trip'
 
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
+  basePath: !debug ? `/${repository}` : '',
   assetPrefix: !debug ? `/${repository}/` : '', // production 일때 prefix 경로
   trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
 }
